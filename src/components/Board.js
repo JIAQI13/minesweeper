26db30e00ebdefae1b5c77109815266ee1e60c88
piggy <jiaqi11@ualberta.ca>
2021-08-05 15:36:42 +0000

    add show all mines after game over

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,6 +5,8 @@ import { revealed } from "../utils/Reveal";
 
 function Board() {
   const [grid, setGrid] = useState([]);
+  const [nonMinecount, setNonMinecount] = useState(0);
+  const [mineLocations, setmineLocations] = useState([]);
 
   const style = {
     display: "flex",
@@ -13,6 +15,8 @@ function Board() {
   useEffect(() => {
     function freshBoard() {
       const newBoard = CreateBoard(16, 16, 40);
+      setNonMinecount(16 * 16 - 40);
+      setmineLocations(newBoard.mineLocation);
       setGrid(newBoard.board);
     }
     freshBoard();
@@ -32,28 +36,38 @@ function Board() {
   const revealcell = (x, y) => {
     let newGrid = JSON.parse(JSON.stringify(grid));
     if (newGrid[x][y].value === "X") {
-      alert("clicked on mine");
+      alert("game over");
+      //show all mines after game over
+      for (let i = 0; i < mineLocations.length; i++) {
+        newGrid[mineLocations[i][0]][mineLocations[i][1]].revealed = true;
+      }
+      setGrid(newGrid);
     } else {
-      let newRevealedBoard = revealed(newGrid, x, y);
-      // newGrid[x][y].revealed = true;
+      let newRevealedBoard = revealed(newGrid, x, y, nonMinecount);
+      setNonMinecount(newRevealedBoard.newNonMinesCount);
       setGrid(newRevealedBoard.arr);
     }
   };
 
-  return grid.map((singleRow) => {
-    return (
-      <div style={style}>
-        {singleRow.map((singleBlock) => {
-          return (
-            <Cell
-              details={singleBlock}
-              updateFlag={updateFlag}
-              revealcell={revealcell}
-            ></Cell>
-          );
-        })}
-      </div>
-    );
-  });
+  return (
+    <>
+      <p>{nonMinecount}</p>
+      {grid.map((singleRow) => {
+        return (
+          <div style={style}>
+            {singleRow.map((singleBlock) => {
+              return (
+                <Cell
+                  details={singleBlock}
+                  updateFlag={updateFlag}
+                  revealcell={revealcell}
+                ></Cell>
+              );
+            })}
+          </div>
+        );
+      })}
+    </>
+  );
 }
 export default Board;
--- src/utils/Reveal.js
+++ src/utils/Reveal.js
@@ -1,7 +1,5 @@
 export const revealed = (arr, x, y, newNonMinesCount) => {
-  console.log(arr[x][y]);
   if (arr[x][y].revealed) {
-    console.log(arr[x][y]);
     return arr;
   }
 
